e51a3f2e483cd2cb999a7104765f74eba60a7380
stephen-standridge <stephen@formisfunction.io>
2016-06-30 21:48:27 -0700

    update repository structure to be more navigable via api request

--- core/webpack.config.production.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-var makeVersionableConfig = require('./makeVersionableConfig');
-var config = require('../../webpack.config.js')
-
-module.exports = makeVersionableConfig(config)
